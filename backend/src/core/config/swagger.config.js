a5477d3537cb63796f95af3ed092f1fa01c67219
Tran Dai Quy <trandaiquy2903@gmail.com>
2024-02-24 10:34:29 +0700

    docs: change name for api docs

--- backend/src/core/config/swagger.config.js
+++ backend/src/core/config/swagger.config.js
@@ -5,7 +5,7 @@ const options = {
     openapi: '3.0.1',
     info: {
         version: '1.0.0',
-        title: 'SafeAlert APIs Document',
+        title: 'SafeAlert APIs Document for Hackathon',
         description: 'API description',
         termsOfService: '',
         contact: {
