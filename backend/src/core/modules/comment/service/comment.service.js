b2eb0bf0c4cffd2bf1f8800249d1614a95f5826f
LongTCH <truongconghoanglong@gmail.com>
2024-03-08 15:36:31 +0700

    feat: add check nested comment in a commentDto and change get comment in Promise all

--- backend/src/core/modules/comment/service/comment.service.js
+++ backend/src/core/modules/comment/service/comment.service.js
@@ -1,6 +1,7 @@
 import { REACTION_OF_TYPE } from 'core/modules/user-react/user-react.const';
 import { CommentDto, PaginationCommentDto } from '../dto';
 import { CommentRepository, UserReactRepository } from '../repository';
+import { COMMENT_PARENT_TYPE } from '../comment.const';
 
 class Service {
     constructor() {
@@ -8,7 +9,7 @@ class Service {
         this.userReactRepository = UserReactRepository;
     }
 
-    async createComment(parentId, parentType, content, userId){
+    async createComment(parentId, parentType, content, userId) {
         await this.commentRepository.insertOne(
             parentId,
             parentType,
@@ -36,19 +37,36 @@ class Service {
         );
         const commentWithReactions = await Promise.all(
             data.map(async e => {
-                const totalLikes = await this.userReactRepository.countLikes(
-                    REACTION_OF_TYPE.COMMENT,
-                    e.id,
-                );
-                const totalDisLikes =
-                    await this.userReactRepository.countDisLikes(
-                        REACTION_OF_TYPE.COMMENT,
-                        e.id,
-                    );
+                const commentsWithTotalLikesAndDislikesAndIfHaveNested =
+                    await Promise.all([
+                        this.userReactRepository.countLikes(
+                            REACTION_OF_TYPE.COMMENT,
+                            e.id,
+                        ),
+                        this.userReactRepository.countDisLikes(
+                            REACTION_OF_TYPE.COMMENT,
+                            e.id,
+                        ),
+                        this.commentRepository.countByParentIdAndType(
+                            e.id,
+                            COMMENT_PARENT_TYPE.COMMENT,
+                        ),
+                    ]);
                 return {
                     ...e,
-                    totalLikes: parseInt(totalLikes.count, 10),
-                    totalDisLikes: parseInt(totalDisLikes.count, 10),
+                    totalLikes: parseInt(
+                        commentsWithTotalLikesAndDislikesAndIfHaveNested[0]
+                            .count,
+                        10,
+                    ),
+                    totalDisLikes: parseInt(
+                        commentsWithTotalLikesAndDislikesAndIfHaveNested[1]
+                            .count,
+                        10,
+                    ),
+                    hasNested:
+                        commentsWithTotalLikesAndDislikesAndIfHaveNested[2]
+                            .count > 0,
                 };
             }),
         );
